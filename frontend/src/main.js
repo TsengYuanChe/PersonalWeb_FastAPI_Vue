6988484e1e2c4cc269028d0a2ab30531833853da
TsengYuanChe <adam880614@gmail.com>
2025-12-02 23:15:25 +0800

    Update: Add css files for RWD

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,13 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 // 自己的全域 CSS
 import './assets/main.css'
+import './assets/main-rwd.css'
 import './assets/projects.css'
+import './assets/projects-rwd.css'
 import './assets/about.css'
+import './assets/about-rwd.css'
 import './assets/exp.css'
+import './assets/exp-rwd.css'
 
 const app = createApp(App)
 
